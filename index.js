af02ab4d572c648ed4740e61e7947133e4f4b34a
Carlos Sujanto <carlos.sujanto@ucalgary.ca>
2024-06-03 11:45:40 -0600

    -index.js, +project.html, button handling

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var divs = document.querySelectorAll('.item-image');
-    divs.forEach(function (div) {
-        var src = div.getAttribute('data-src');
-        div.style.backgroundImage = "url('" + src + "')";
-    });
-});
